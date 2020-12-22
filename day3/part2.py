25b34e806c2612a7ecddbe6fd322ecb87c9dd182
leofernandeso <leonardo.fernandes.oliv@gmail.com>
2020-12-22 13:55:14 -0300

    finished day 3

---
+++ day3/part2.py
@@ -0,0 +1,36 @@
+import sys
+from functools import reduce
+
+def parse_input():
+    path = []
+    for line in sys.stdin:
+        path.append(list(line.strip()))
+    return path
+
+def count_trees(path, slope):
+    nrows, ncols = len(path), len(path[0])
+    slope_i, slope_j = slope
+    i = 0
+    j = 0
+    tree_count = 0
+    while (i < nrows):
+        j = j % ncols
+
+        if path[i][j] == '#': 
+            # found a tree
+            tree_count += 1
+
+        # move
+        i += slope_i
+        j += slope_j
+
+    return tree_count
+
+path = parse_input()
+slopes = [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]
+counts = [count_trees(path, slope) for slope in slopes]
+result = reduce(lambda x, y: x * y, counts)
+print(result)
+
+    
+
